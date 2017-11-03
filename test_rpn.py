266b68e5e484e91362c318431de28360e55647a2
Biswash Ghimire <biswash@umich.edu>
2017-11-03 16:36:58 -0400

    comment

--- test_rpn.py
+++ test_rpn.py
@@ -3,6 +3,7 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
+    #addition test
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
